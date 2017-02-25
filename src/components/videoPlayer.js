1c6e433354d02a59b5fdeecfdb33efd7e0257196
edwinbrower <edwin.brower@gmail.com>
2017-02-24 21:40:59 -0800

    Tests through video player complete!

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -2,6 +2,14 @@ angular.module('video-player')
 
 .directive('videoPlayer', function() {
   return {
+    scope: {
+      video: '<',
+    },
+    controllerAs: 'ctrl',
+    bindToController: true,
+    controller: function($scope) {
+      console.log('Player: ', $scope);
+    },
     templateUrl: 'src/templates/videoPlayer.html'
   };
 });
