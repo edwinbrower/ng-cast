b0bc87b439a8effbb856b78e8404b726a9395aee
edwinbrower <edwin.brower@gmail.com>
2017-02-25 12:59:10 -0800

    Code cleanup

--- src/components/app.js
+++ src/components/app.js
@@ -1,42 +1,27 @@
 angular.module('video-player')  // square brackets????? 
-// .run('youTube', function() {
-//   var YouTube = new youTube();
-//   // var searchResults = YouTube.search();
-// })
+
 .directive('app', function() {
 
   return {
-    
-    controller: function($window, youTube) {
-      var ctrl = this;
-      // console.log(this);
-      ctrl.videos = window.exampleVideoData;
-      ctrl.currentVideo = window.exampleVideoData[0];
-      ctrl.selectVideo = function() {};
-      ctrl.searchResults = function() {};
-      ctrl.onClick = function(index) {
+
+    controller: function() {
+      this.videos = window.exampleVideoData;
+      this.currentVideo = window.exampleVideoData[0];
+      this.selectVideo = function() {};
+      this.searchResults = function() {};
+      this.onClick = function(index) {
         console.log('index ', index);
-        ctrl.currentVideo = ctrl.videos[index];
-      };
-      ctrl.search = youTube.search;
-      // ctrl.search();
-      console.log(youTube.search);
+        this.currentVideo = this.videos[index];
+      }.bind(this);
+      // ctrl.search = youTube.search;
+      // // ctrl.search();
+      // console.log(youTube.search);
       // ctrl.$inject = [$scope, 'youTube'];
     },
     scope: {},
     controllerAs: 'ctrl',
     bindToController: true,
     templateUrl: 'src/templates/app.html'
-    // scope: {
-    // //   // ctrl: this.controller
-    // }, 
-// .controller('AppController', function($scope) {
-//   $scope.videos = window.exampleVideoData;
-//   $scope.video = window.exampleVideoData[0];
-//   // $scope.selectVideo = function() {};
-//   // $scope.searchResults = function() {};
-//   console.log('app ', $scope);
-//   // $scope.
-// })
+
   };
 });
